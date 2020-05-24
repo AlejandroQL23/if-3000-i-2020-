5bac8cb84de424e1e5c9d8477282062331c955f2
DiegoGarita <62150768+DiegoGarita@users.noreply.github.com>
2020-05-23 18:27:04 -0600

    Update Main.java

--- labs/labprueba/nBtest/src/main/java/edu/ucr/rp/nbtest/Main.java
+++ labs/labprueba/nBtest/src/main/java/edu/ucr/rp/nbtest/Main.java
@@ -6,7 +6,8 @@ public class Main {
   
     public static void main(String[] args) {
 
-        System.out.println("HOLA MUNDO");
+        System.out.println("HOLA MUNDOs");
+      
         
     }
     
