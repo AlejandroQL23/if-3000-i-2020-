6cf3da61bf6870f92751d46e4c8643f54c90026e
DiegoGarita <diegogarita2413@gmail.com>
2020-05-23 19:12:05 -0600

    commit?

--- PrimerProyecto/AppManejoDeInventarios/src/main/java/edu/ucr/rp/appmanejodeinventarios/InventoryApplicationDemo.java
+++ PrimerProyecto/AppManejoDeInventarios/src/main/java/edu/ucr/rp/appmanejodeinventarios/InventoryApplicationDemo.java
@@ -6,6 +6,7 @@ public class InventoryApplicationDemo {
 
     public static void main(String[] args) {
         System.out.println("PROYECTO FINALIZADO!!!");
+        System.out.println("Update");
     }
     
 }
