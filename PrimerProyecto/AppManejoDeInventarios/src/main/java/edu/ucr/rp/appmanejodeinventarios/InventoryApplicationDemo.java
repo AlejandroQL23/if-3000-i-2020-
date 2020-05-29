749bdb3349ff0059d89d8155beafcc3c4193f612
Alejandro Quesada Leiva <ale23200@gmail.com>
2020-05-29 17:42:47 -0600

    prueba buena

--- PrimerProyecto/AppManejoDeInventarios/src/main/java/edu/ucr/rp/appmanejodeinventarios/InventoryApplicationDemo.java
+++ PrimerProyecto/AppManejoDeInventarios/src/main/java/edu/ucr/rp/appmanejodeinventarios/InventoryApplicationDemo.java
@@ -7,6 +7,7 @@ public class InventoryApplicationDemo {
     public static void main(String[] args) {
         System.out.println("PROYECTO FINALIZADO!!!");
         System.out.println("Update");
+        System.out.println("HOLA DIEGO GARITA");
     }
     
 }
